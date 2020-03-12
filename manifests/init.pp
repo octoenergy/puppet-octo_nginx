8e8e2137b9385ffdfdc62b2e2e5e491793dcc752
David Winterbottom <david.winterbottom@gmail.com>
2020-03-12 20:12:37 +0000

    Make default site config optional
    As there are some cases where we want it managed by consul-template.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class octo_nginx (
     # Use custom PPA to get modern version of Nginx
     include apt
     apt::ppa { "ppa:nginx/stable": }
-    exec { "update package lists for nginx": 
+    exec { "update package lists for nginx":
         command => "/usr/bin/apt-get update",
         require => Apt::Ppa["ppa:nginx/stable"]
     }
@@ -13,26 +13,34 @@ class octo_nginx (
         require => Exec["update package lists for nginx"],
     }
 
-    file { "nginx config": 
+    file { "nginx config":
         path => "/etc/nginx/nginx.conf",
         ensure => "file",
         source => "puppet:///modules/octo_nginx/nginx.conf",
         require => Package["nginx"],
     }
 
-    file { "nginx default site": 
-        path => "/etc/nginx/sites-enabled/default",
-        ensure => "file",
-        source => $default_site_source,
-        require => Package["nginx"],
-    }
-
-    service { "nginx":
-        ensure => running,
-        subscribe => [
-            File["nginx config"],
-            File["nginx default site"],
-        ],
+    if $default_site_source {
+        file { "nginx default site":
+            path => "/etc/nginx/sites-enabled/default",
+            ensure => "file",
+            source => $default_site_source,
+            require => Package["nginx"],
+        }
+        service { "nginx":
+            ensure => running,
+            subscribe => [
+                File["nginx config"],
+                File["nginx default site"],
+            ],
+        }
+    } else {
+        service { "nginx":
+            ensure => running,
+            subscribe => [
+                File["nginx config"],
+            ],
+        }
     }
 
 }
